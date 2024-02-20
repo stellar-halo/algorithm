662779733ea4ac2dc5702d9c4cfcbe7a21685ac4
Hailey <91793891+stellar-halo@users.noreply.github.com>
2024-02-21 04:30:50 +0900

    [Silver V] Title: 폴리오미노, Time: 124 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.java
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -1,23 +1,56 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Objects;
 
 public class Main {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        String board = sc.next();
-        sc.close();
-        String result = "";
-        result = poliomino(board);
-        System.out.println(result);
-    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    private static String poliomino(String board) {
-        String ans = "";
-        String A = "AAAA", B = "BB";
-        board = board.replaceAll("XXXX", A);
-        ans = board.replaceAll("XX", B);
-        if (ans.contains("X")) {
-            ans = "-1";
+        String board = br.readLine();
+        int boardSize = board.length();
+        int count = 0;
+        String[] boardSpaces = board.split("");
+
+        for (int idx = 0; idx < boardSize; idx++) {
+            if (Objects.equals(boardSpaces[idx], "X")) { //X라면
+                count++; //X개수 하나 올리고
+                boardSpaces[idx] = "A";
+                if (idx == boardSize - 1) { //X만 나오다가 끝나는 경우
+                    if (count % 2 == 0) { //count가 짝수인지 판별
+                        int bCount = (count % 4) / 2; //b는 어짜피 0개 아니면 1개이다. b는 몇 개인지
+                        for (int temp = bCount * 2; temp > 0; temp--) {
+                            boardSpaces[idx - temp + 1] = "B";
+                        }
+                    }
+                }
+            } else { //.이 나온다면
+                if (count % 2 == 0) { //count가 짝수인지 판별
+                    int bCount = (count % 4) / 2; //b는 어짜피 0개 아니면 1개이다. b는 몇 개인지
+                    for (int temp = bCount * 2; temp > 0; temp--) {
+                        boardSpaces[idx - temp] = "B";
+                    }
+                    //초기화
+                    count = 0;
+                } else { //홀수개 for문 중단
+                    break;
+                }
+            }
         }
-        return ans;
+
+        if (count%2==0) {
+            for (String item : boardSpaces) {
+                bw.write(item + "");
+            }
+        }
+        else{
+            bw.write(-1+"");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
