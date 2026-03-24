8d1d5bd87e14a7a73e2efb5718fecad24a219c06
Hailey <91793891+stellar-halo@users.noreply.github.com>
2026-03-24 22:13:44 +0900

    [Silver III] Title: 타노스, Time: 116 ms, Memory: 14348 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.java
+++ 백준/Silver/20310. 타노스/타노스.java
@@ -15,18 +15,32 @@ public class Main {
                 zero++;
             }
         }
-        StringBuilder sb = new StringBuilder();
+
         one = one / 2;
         zero = zero / 2;
 
-        while (zero > 0) {
-            zero--;
-            sb.append("0");
+        for (int idx = 0; idx < S.length; idx++) {
+            if (one > 0 && S[idx].equals("1")) {
+                S[idx] = "";
+                one--;
+            }
+
+            if(one == 0) break;
         }
 
-        while (one > 0) {
-            one--;
-            sb.append("1");
+        for (int idx = S.length - 1; idx >= 0; idx--) {
+            if (zero > 0 && S[idx].equals("0")) {
+                S[idx] = "";
+                zero--;
+            }
+
+            if(zero == 0) break;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int idx = 0; idx < S.length; idx++) {
+            sb.append(S[idx]);
         }
 
         System.out.println(sb);
