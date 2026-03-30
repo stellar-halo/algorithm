3bc4141d8fa13ede5e526b4737cdde67c2bcb724
Hailey <91793891+stellar-halo@users.noreply.github.com>
2026-03-30 21:42:59 +0900

    [Silver III] Title: 주유소, Time: 316 ms, Memory: 30360 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] price = new int[N];
+        int[] distance = new int[N-1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int n = 0; n < N - 1; n++) {
+            int d = Integer.parseInt(st.nextToken());
+            distance[n] = d;
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int n = 0; n < N ; n++) {
+            int p = Integer.parseInt(st.nextToken());
+            price[n] = p;
+        }
+
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+
+        for (int n = 0; n < N - 1; n++) {
+            if (price[n] < min) {
+                min = price[n];
+            }
+            sum += min * distance[n];
+        }
+
+        System.out.println(sum);
+    }
+}
+
